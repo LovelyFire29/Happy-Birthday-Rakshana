b2d6454e6990fc080e3cb3fbce6fb379d963bb94
LovelyFire29 <ssrinivasaraghavan29@gmail.com>
2025-12-04 20:26:50 +0530

    first commit

---
+++ Rakshana-Birthday-code-project/src/data.js
@@ -0,0 +1,57 @@
+// ==========================
+// 1. Intro Screen
+// ==========================
+export const firstHeading = "A Goated Friend was born today, 18 years ago!"
+// Main heading text - Change this text as you like
+
+export const firstSubtext = "Yes, it’s YOU! A little surprise awaits..."
+// Subtext under heading (optional) - Change, leave blank or remove.
+
+
+// ==========================
+// 2. Cake Screen
+// ==========================
+// Add recipient's name here.
+export const NAME = "Rakshana!"
+
+
+// ==========================
+// 3. Photo Screen
+// ==========================
+export const photoScreenHeading = "Moments Worth Keeping"
+// Heading above photos - Change this text as you like
+
+// Add your photos inside "public/images" folder
+// Example: { "/images/1.jpeg" }, Match the format and add more as needed
+export const photos = [
+    "/images/1.jpeg",
+    "/images/2.jpeg",
+    "/images/3.jpeg",
+    "/images/4.jpeg",
+]
+
+
+// ==========================
+// 4. Message Screen
+// ==========================
+export const messageScreenHeading = "A Few Words for You"
+// Heading above card - Change this text as you like
+export const specialMessage = 'Innum Yosikala'
+// Main special message (you can edit freely)
+
+
+// ==========================
+// 4. Final Screen
+// ==========================
+export const finalScreenHeading = "Before You Go…"
+// Heading above final surprise - Change this text as you like
+
+export const overlayText = "Thanks for being the person who reminded me to look at the good again."
+// First text inside the overlay card - Change this text as you like
+
+export const overlayMessage = "Happy birthday.And yeah… I hope this Brahmastra (surprise) landed."
+// ==========================
+// Background Music
+// ==========================
+// Place your .mp3 file inside "public/audio" and update path below
+export const backgroundMusic = "./audio/bg.mp3"
